3ff078937b62e31917d84d79b0b8660120e8172d
Joshua Powers <josh.powers@canonical.com>
2018-01-02 12:28:33 -0800

    Improve the upload report output
    1) drop already released uploads from proposed
    
    This makes the list more readable by eliminating redundancy.
    And it makes the list in proposed really the one we want/need help on
    testing as the others are already out.
    
    Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>
    
    2) improve headers for Proposed and SRU
    
    In proposed we want to call for support and in SRU we want to make it
    more clear that this completed already.
    
    Fixes 10
    
    Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>
    
    3) Link to uploads of uploads
    
    By that people can easily get to the changed content as well as from
    there to the full bug descriptions related to the upload.
    
    Fixes 9
    
    Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>

--- upload_report.py
+++ upload_report.py
@@ -6,10 +6,13 @@ Robbie Basak <robie.basak@canonical.com>
 Joshua Powers <josh.powers@canonical.com>
 """
 from functools import lru_cache
-import itertools
 import sys
 
 import launchpadlib.launchpad
+from lazr.restfulclient.errors import ClientError
+
+SRU = 'SRU'
+DEV = 'DEV'
 
 
 @lru_cache()
@@ -22,7 +25,11 @@ def series_name(launchpad, series_link):
 def person_name(launchpad, person_link):
     """Translate name link to a name."""
     if person_link:
-        return launchpad.load(person_link).name
+        try:
+            return launchpad.load(person_link).name
+        except ClientError as exc:
+            if exc.response["status"] == "410":  # gone, user suspended
+               return None
     return None
 
 
@@ -55,6 +62,7 @@ def generate_uploads(start_date):
             report_entry = {
                 'package': spph.source_package_name,
                 'version': spph.source_package_version,
+                'author': person_name(launchpad, spph.package_creator_link),
                 'series': series_name(launchpad, spph.distro_series_link),
                 'signer': person_name(launchpad, spph.package_signer_link),
                 'sponsor': person_name(launchpad, spph.sponsor_link),
@@ -73,9 +81,15 @@ def generate_uploads(start_date):
                     report_entry['migrated'] = (
                         report_entry['version'] in migrated_versions
                     )
-                report_entry['category'] = 'Development release'
+                report_entry['category'] = DEV
             else:
-                report_entry['category'] = 'SRU'
+                if report_entry['pocket'] == 'Updates':
+                    migrated_versions.add(report_entry['version'])
+                elif report_entry['pocket'] == 'Proposed':
+                    report_entry['migrated'] = (
+                        report_entry['version'] in migrated_versions
+                    )
+                report_entry['category'] = SRU
 
             if report_entry['sponsor'] == 'ubuntu-archive-robot':
                 continue  # skip syncs
@@ -83,29 +97,57 @@ def generate_uploads(start_date):
             yield report_entry
 
 
+def print_proposed_sru(entries):
+    """Print SRU Proposed entries."""
+    print('')
+    print('### Proposed Uploads to the Supported Releases')
+    print('Please consider testing the following by [enabling proposed]'
+          '(https://wiki.ubuntu.com/Testing/EnableProposed)'
+          ', checking packages for update regressions, and making sure to '
+          'mark affected bugs [verified as fixed]'
+          '(https://wiki.ubuntu.com/StableReleaseUpdates#Verification).\n')
+    print('Total: %s' % len(entries))
+
+    if not entries:
+        return
+
+    for entry in entries:
+        uploader = entry['author'] if entry['author'] else entry['signer']
+        print('[`%s, %s, %s, %s`](https://launchpad.net/ubuntu/+source/%s/%s)' %
+              (entry['package'], entry['series'], entry['version'], uploader,
+               entry['package'], entry['version']))
+
+
 def print_sru(entries):
     """Print SRU entries."""
     print('')
-    print('### Uploads to the Development Release')
-    print('```')
-    for entry in entries:
-        print('%s, %s, %s, %s' % (entry['package'], entry['series'],
-                                  entry['version'], entry['signer']))
+    print('### Uploads released to the Supported Releases')
     print('Total: %s' % len(entries))
-    print('```')
+
+    if not entries:
+        return
+
+    for entry in entries:
+        uploader = entry['author'] if entry['author'] else entry['signer']
+        print('[`%s, %s, %s, %s`](https://launchpad.net/ubuntu/+source/%s/%s)' %
+              (entry['package'], entry['series'], entry['version'], uploader,
+               entry['package'], entry['version']))
 
 
 def print_dev(entries):
     """Print Dev release entries."""
     print('')
-    print('### Uploads to the Supported Releases')
-    print('```')
-    for entry in entries:
-        print('%s, %s, %s' % (entry['package'], entry['version'],
-                              entry['signer']))
+    print('### Uploads to the Development Release')
     print('Total: %s' % len(entries))
-    print('```')
 
+    if not entries:
+        return
+
+    for entry in entries:
+        uploader = entry['author'] if entry['author'] else entry['signer']
+        print('[`%s, %s, %s`](https://launchpad.net/ubuntu/+source/%s/%s)' %
+              (entry['package'], entry['version'], uploader,
+               entry['package'], entry['version']))
 
 def main():
     """Get report and print."""
@@ -114,16 +156,12 @@ def main():
         key=lambda r: r['category']
     )
 
-    grouped_report = itertools.groupby(
-        report,
-        key=lambda r: r['category'],
-    )
-
-    for category, entries in grouped_report:
-        if category == 'SRU':
-            print_sru(list(entries))
-        else:
-            print_dev(list(entries))
+    print_proposed_sru([item for item in report if item['category'] == SRU
+                       and item['pocket'] == 'Proposed'
+                       and not item['migrated']])
+    print_sru([item for item in report if item['category'] == SRU
+              and item['pocket'] == 'Updates'])
+    print_dev([item for item in report if item['category'] == DEV])
 
 
 if __name__ == '__main__':
