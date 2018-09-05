39620d3ff063e006b054c12fb2c270d980755299
Joshua Powers <josh.powers@canonical.com>
2018-09-05 08:57:52 -0700

    Update backlog code to no longer use prometheus

--- report_backlog.py
+++ report_backlog.py
@@ -9,38 +9,20 @@ import json
 import sys
 from urllib.request import urlopen
 
-BASE_URL = 'http://10.25.2.224:9090/api/v1/query?query='
-
-
-def _get_json_from_url(json_url):
-    """Return JSON from a URL."""
-    try:
-        with urlopen(json_url) as url:
-            data = json.loads(url.read().decode())
-    except TimeoutError:
-        sys.exit(1)
-
-    return data
+from launchpadlib.launchpad import Launchpad
 
 
 def bug_backlog_size():
     """Print out the current backlog size."""
-    query = 'sum(server_triage_backlog_total{exported_job="server-triage"})'
-    data = _get_json_from_url('%s%s' % (BASE_URL, query))
-    backlog = data['data']['result'][0]['value'][1]
-    print("- %s in the [backlog]('https://bugs.launchpad.net/"
+    launchpad = Launchpad.login_anonymously(
+        'ubuntu-bug-triage', 'production', version='devel'
+    )
+    ubuntu = launchpad.distributions['Ubuntu']
+    team = launchpad.people['ubuntu-server']
+    backlog = len(ubuntu.searchTasks(bug_subscriber=team))
+
+    print("- %s bugs in the [backlog]('https://bugs.launchpad.net/"
           "~ubuntu-server/+subscribedbugs)" % backlog)
-
-
-def bug_reviewed_size(start_date):
-    """Print number of bugs reviewed since last summary."""
-    date_fmt = '%Y-%m-%d'
-    time_difference = datetime.now() - datetime.strptime(start_date, date_fmt)
-    query = ('sum(increase(server_triage_daily_triage_total{exported_job='
-             '"server-triage"}[%sd]))' % time_difference.days)
-    data = _get_json_from_url('%s%s' % (BASE_URL, query))
-    reviewed = int(float(data['data']['result'][0]['value'][1]))
-    print("- %s bugs reviewed since the last report" % reviewed)
 
 
 def main():
@@ -48,14 +30,9 @@ def main():
     print('')
     print('## Bug Work and Triage\n')
     bug_backlog_size()
-    bug_reviewed_size(sys.argv[1])
     print('- [Notes on daily bug triage](https://wiki.ubuntu.com/'
           'ServerTeam/KnowledgeBase#Bug_Triage)\n')
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Please enter a date in the format YYYY-MM-DD')
-        sys.exit(1)
-
     main()
